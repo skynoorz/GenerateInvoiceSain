55b03c1395993813bf612a6685156d692ea421f6
skynoorz <aligatorbol@gmail.com>
2022-04-12 10:29:38 -0400

    first commit

---
+++ src/main/java/com/tigo/bo/GenerateInvoiceSain/GenerateInvoiceSainApplication.java
@@ -0,0 +1,13 @@
+package com.tigo.bo.GenerateInvoiceSain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerateInvoiceSainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerateInvoiceSainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tigo/bo/GenerateInvoiceSain/GenerateInvoiceSainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tigo.bo.GenerateInvoiceSain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerateInvoiceSainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
