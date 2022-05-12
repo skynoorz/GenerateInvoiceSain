37a7dfbf8abc7b53acf212250f4edbf7912f57e8
skynoorz <aligatorbol@gmail.com>
2022-05-12 02:10:18 -0400

    classes generated, delete country classes

--- src/main/java/com/tigo/bo/GenerateInvoiceSain/WebServiceConfig.java
+++ src/main/java/com/tigo/bo/GenerateInvoiceSain/WebServiceConfig.java
@@ -33,8 +33,43 @@ public class WebServiceConfig extends WsConfigurerAdapter {
         return wsdl11Definition;
     }
 
+    @Bean(name = "generateInvoiceSainRequest")
+    public DefaultWsdl11Definition defaultWsdl11(XsdSchema generateInvoiceSainRequestSchema) {
+        DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+        wsdl11Definition.setPortTypeName("GenerateInvoicePort");
+        wsdl11Definition.setLocationUri("/ws");
+        wsdl11Definition.setTargetNamespace("http://xmlns.tigo.com/SAIN/GenerateInvoiceSainRequest/V1/schema");
+        wsdl11Definition.setSchema(generateInvoiceSainRequestSchema);
+        return wsdl11Definition;
+    }
+
     @Bean
     public XsdSchema countriesSchema() {
         return new SimpleXsdSchema(new ClassPathResource("countries.xsd"));
+    }
+
+    @Bean
+    public XsdSchema generateInvoiceSainRequestSchema() {
+        return new SimpleXsdSchema(new ClassPathResource("GenerateInvoiceSainRequest.xsd"));
+    }
+
+    @Bean
+    public XsdSchema generateInvoiceSainResponseSchema() {
+        return new SimpleXsdSchema(new ClassPathResource("GenerateInvoiceSainResponse.xsd"));
+    }
+
+    @Bean
+    public XsdSchema parameterTypeV2Schema() {
+        return new SimpleXsdSchema(new ClassPathResource("ParameterTypeV2.xsd"));
+    }
+
+    @Bean
+    public XsdSchema requestHeaderSchema() {
+        return new SimpleXsdSchema(new ClassPathResource("RequestHeaderV3.xsd"));
+    }
+
+    @Bean
+    public XsdSchema responseHeaderV3Schema() {
+        return new SimpleXsdSchema(new ClassPathResource("ResponseHeaderV3.xsd"));
     }
 }
